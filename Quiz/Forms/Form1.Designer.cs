c9b7757b2a3074b33eb7db8f96a261e57f4c3a96
naingkokolin <naingkokolin007@gmail.com>
2026-01-26 15:52:46 +0630

    made beta version for production stage

--- Quiz/Forms/Form1.Designer.cs
+++ Quiz/Forms/Form1.Designer.cs
@@ -38,16 +38,28 @@
             lblOptionB = new Label();
             lblOptionC = new Label();
             lblOptionD = new Label();
+            pictureBox1 = new PictureBox();
+            pbTimer = new ProgressBar();
+            btnStop = new Button();
+            lblCorrectAnswer = new Label();
+            btnJetpot = new Button();
+            panelWelcome = new Panel();
+            btnStartQuiz = new Button();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panelWelcome.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // lblTimer
             // 
             lblTimer.AutoSize = true;
-            lblTimer.Location = new Point(1286, 125);
+            lblTimer.Font = new Font("MS Reference Sans Serif", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTimer.Location = new Point(1558, 792);
             lblTimer.Name = "lblTimer";
-            lblTimer.Size = new Size(59, 25);
+            lblTimer.Size = new Size(306, 118);
             lblTimer.TabIndex = 1;
-            lblTimer.Text = "label1";
+            lblTimer.Text = "timer";
             // 
             // timer1
             // 
@@ -57,17 +69,18 @@
             // lblQuestion
             // 
             lblQuestion.AutoSize = true;
-            lblQuestion.Location = new Point(273, 67);
+            lblQuestion.Font = new Font("Arial Narrow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblQuestion.Location = new Point(342, 49);
             lblQuestion.Name = "lblQuestion";
-            lblQuestion.Size = new Size(59, 25);
+            lblQuestion.Size = new Size(184, 57);
             lblQuestion.TabIndex = 2;
-            lblQuestion.Text = "label1";
+            lblQuestion.Text = "Question";
             // 
             // btnShowAnswer
             // 
-            btnShowAnswer.Location = new Point(319, 362);
+            btnShowAnswer.Location = new Point(334, 948);
             btnShowAnswer.Name = "btnShowAnswer";
-            btnShowAnswer.Size = new Size(154, 34);
+            btnShowAnswer.Size = new Size(167, 53);
             btnShowAnswer.TabIndex = 3;
             btnShowAnswer.Text = "Show Answer";
             btnShowAnswer.UseVisualStyleBackColor = true;
@@ -75,9 +88,9 @@
             // 
             // btnNext
             // 
-            btnNext.Location = new Point(881, 402);
+            btnNext.Location = new Point(613, 948);
             btnNext.Name = "btnNext";
-            btnNext.Size = new Size(112, 34);
+            btnNext.Size = new Size(134, 53);
             btnNext.TabIndex = 4;
             btnNext.Text = "Next";
             btnNext.UseVisualStyleBackColor = true;
@@ -86,44 +99,135 @@
             // lblOptionA
             // 
             lblOptionA.AutoSize = true;
-            lblOptionA.Location = new Point(343, 168);
+            lblOptionA.Font = new Font("Arial Narrow", 24F);
+            lblOptionA.Location = new Point(344, 369);
             lblOptionA.Name = "lblOptionA";
-            lblOptionA.Size = new Size(24, 25);
+            lblOptionA.Size = new Size(51, 57);
             lblOptionA.TabIndex = 5;
             lblOptionA.Text = "A";
             // 
             // lblOptionB
             // 
             lblOptionB.AutoSize = true;
-            lblOptionB.Location = new Point(345, 214);
+            lblOptionB.Font = new Font("Arial Narrow", 24F);
+            lblOptionB.Location = new Point(344, 518);
             lblOptionB.Name = "lblOptionB";
-            lblOptionB.Size = new Size(22, 25);
+            lblOptionB.Size = new Size(51, 57);
             lblOptionB.TabIndex = 6;
             lblOptionB.Text = "B";
             // 
             // lblOptionC
             // 
             lblOptionC.AutoSize = true;
-            lblOptionC.Location = new Point(345, 259);
+            lblOptionC.Font = new Font("Arial Narrow", 24F);
+            lblOptionC.Location = new Point(342, 660);
             lblOptionC.Name = "lblOptionC";
-            lblOptionC.Size = new Size(23, 25);
+            lblOptionC.Size = new Size(53, 57);
             lblOptionC.TabIndex = 7;
             lblOptionC.Text = "C";
             // 
             // lblOptionD
             // 
             lblOptionD.AutoSize = true;
-            lblOptionD.Location = new Point(345, 299);
+            lblOptionD.Font = new Font("Arial Narrow", 24F);
+            lblOptionD.Location = new Point(342, 792);
             lblOptionD.Name = "lblOptionD";
-            lblOptionD.Size = new Size(25, 25);
+            lblOptionD.Size = new Size(53, 57);
             lblOptionD.TabIndex = 8;
             lblOptionD.Text = "D";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.mst_logo_png;
+            pictureBox1.Location = new Point(12, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(294, 359);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // pbTimer
+            // 
+            pbTimer.Location = new Point(1541, 948);
+            pbTimer.Maximum = 60;
+            pbTimer.Name = "pbTimer";
+            pbTimer.Size = new Size(323, 49);
+            pbTimer.Style = ProgressBarStyle.Continuous;
+            pbTimer.TabIndex = 10;
+            pbTimer.Value = 60;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(872, 948);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(134, 53);
+            btnStop.TabIndex = 11;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // lblCorrectAnswer
+            // 
+            lblCorrectAnswer.AutoSize = true;
+            lblCorrectAnswer.Font = new Font("Arial Narrow", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCorrectAnswer.Location = new Point(680, 323);
+            lblCorrectAnswer.Name = "lblCorrectAnswer";
+            lblCorrectAnswer.Size = new Size(444, 83);
+            lblCorrectAnswer.TabIndex = 12;
+            lblCorrectAnswer.Text = "Correct Answer";
+            // 
+            // btnJetpot
+            // 
+            btnJetpot.Enabled = false;
+            btnJetpot.Location = new Point(1092, 948);
+            btnJetpot.Name = "btnJetpot";
+            btnJetpot.Size = new Size(175, 53);
+            btnJetpot.TabIndex = 13;
+            btnJetpot.Text = "Jetpot Question";
+            btnJetpot.UseVisualStyleBackColor = true;
+            // 
+            // panelWelcome
+            // 
+            panelWelcome.Controls.Add(btnStartQuiz);
+            panelWelcome.Controls.Add(pictureBox2);
+            panelWelcome.Dock = DockStyle.Fill;
+            panelWelcome.Location = new Point(0, 0);
+            panelWelcome.Name = "panelWelcome";
+            panelWelcome.Size = new Size(1910, 1071);
+            panelWelcome.TabIndex = 16;
+            // 
+            // btnStartQuiz
+            // 
+            btnStartQuiz.Location = new Point(843, 845);
+            btnStartQuiz.Name = "btnStartQuiz";
+            btnStartQuiz.Size = new Size(200, 61);
+            btnStartQuiz.TabIndex = 1;
+            btnStartQuiz.Text = "Start Quiz";
+            btnStartQuiz.UseVisualStyleBackColor = true;
+            btnStartQuiz.Click += btnStartQuiz_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.home;
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(1920, 1080);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1489, 595);
+            AutoSize = true;
+            ClientSize = new Size(1910, 1071);
+            Controls.Add(panelWelcome);
+            Controls.Add(btnJetpot);
+            Controls.Add(lblCorrectAnswer);
+            Controls.Add(btnStop);
+            Controls.Add(pbTimer);
+            Controls.Add(pictureBox1);
             Controls.Add(lblOptionD);
             Controls.Add(lblOptionC);
             Controls.Add(lblOptionB);
@@ -133,7 +237,12 @@
             Controls.Add(lblQuestion);
             Controls.Add(lblTimer);
             Name = "Form1";
-            Text = "Form1";
+            Text = "M.S.T College Quiz";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panelWelcome.ResumeLayout(false);
+            panelWelcome.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -148,5 +257,13 @@
         private Label lblOptionB;
         private Label lblOptionC;
         private Label lblOptionD;
+        private PictureBox pictureBox1;
+        private ProgressBar pbTimer;
+        private Button btnStop;
+        private Label lblCorrectAnswer;
+        private Button btnJetpot;
+        private Panel panelWelcome;
+        private Button btnStartQuiz;
+        private PictureBox pictureBox2;
     }
 }
--- Quiz/Forms/Form1.cs
+++ Quiz/Forms/Form1.cs
@@ -13,12 +13,16 @@ namespace Quiz
     {
         private Random rng = new Random();
         private int timeLeft = 60;
-        private int questionsPerSession = 40; // Number of random questions to pick
+        private int timer = 60;
+        private int questionsPerSession = 52; // Number of random questions to pick
+        private bool alarmPlayed = false;
+        private bool isJetpotMode = false;
 
         private List<QuestionGroup> allGroups = new List<QuestionGroup>();
         private List<Question> activeQuizPool = new List<Question>();
         private List<string> usedQuestionIds = new List<string>();
         private List<Label> optionLabels;
+        private List<string> sessionResults = new List<string>();
 
         private int currentIndex = 0;
         private string usedFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "used_questions.txt");
@@ -27,48 +31,114 @@ namespace Quiz
         {
             InitializeComponent();
 
-            // Link UI buttons (Ensure these names match your Designer names)
+            timer1.Stop();
+            timer1.Enabled = false;
+
+            btnJetpot.Click += new EventHandler(btnJetpot_Click);
+
             optionLabels = new List<Label> { lblOptionA, lblOptionB, lblOptionC, lblOptionD };
 
             // Initial UI Setup
             btnNext.Visible = false;
+            btnStop.Visible = false;
             btnShowAnswer.Visible = false;
+            lblCorrectAnswer.Visible = false;
+            btnJetpot.Visible = false;
 
-            StartNewSession();
+            panelWelcome.Visible = true;
+            panelWelcome.BringToFront();
+
+            // StartNewSession();
         }
+
+        //private void StartNewSession()
+        //{
+        //    LoadAllQuestions();
+        //    LoadUsedHistory();
+        //    isJetpotMode = false;
+
+        //    // 1. Get the Sample Questions first (and filter out used ones)
+        //    var samplePool = allGroups
+        //        .Where(g => g.GroupName == "SampleQuestions")
+        //        .SelectMany(g => g.Questions)
+        //        .Where(q => !usedQuestionIds.Contains(q.Id))
+        //        .ToList();
+
+        //    // 2. Get the rest of the questions from other groups
+        //    var otherPool = allGroups
+        //        .Where(g => g.GroupName != "SampleQuestions")
+        //        .SelectMany(g => g.Questions)
+        //        .Where(q => !usedQuestionIds.Contains(q.Id))
+        //        .OrderBy(x => rng.Next()) // Shuffle only the non-sample questions
+        //        .ToList();
+
+        //    // 3. Combine them: Sample questions first, then the random ones
+        //    activeQuizPool = samplePool.Concat(otherPool).Take(questionsPerSession).ToList();
+
+        //    if (activeQuizPool.Count > 0)
+        //    {
+        //        LoadQuestion();
+        //    }
+        //    else
+        //    {
+        //        lblQuestion.Text = "No new questions left!";
+        //        MessageBox.Show("All questions used. Add New Questions or Reset the history file.");
+        //    }
+        //}
 
         private void StartNewSession()
         {
-            LoadAllQuestions();
+            isJetpotMode = false;
+            LoadAllQuestions("questions.json");
             LoadUsedHistory();
 
-            // 1. Merge all questions from all groups
-            // 2. Filter out questions already listed in used_questions.txt
-            // 3. Shuffle the remaining pool
-            // 4. Take the session count (e.g., 40)
-            activeQuizPool = allGroups
+            // 1. Get Samples
+            var samplePool = allGroups
+                .Where(g => g.GroupName == "SampleQuestions")
+                .SelectMany(g => g.Questions)
+                .Where(q => !usedQuestionIds.Contains(q.Id)).ToList();
+
+            // 2. Get Others
+            var otherPool = allGroups
+                .Where(g => g.GroupName != "SampleQuestions")
                 .SelectMany(g => g.Questions)
                 .Where(q => !usedQuestionIds.Contains(q.Id))
-                .OrderBy(x => rng.Next())
-                .Take(questionsPerSession)
-                .ToList();
+                .OrderBy(x => rng.Next()).ToList();
+
+            // 3. Combine for 50 total
+             //activeQuizPool = samplePool.Concat(otherPool).Take(questionsPerSession).ToList();
+            activeQuizPool = samplePool.Take(2).Concat(otherPool.Take(60)).ToList();
 
             if (activeQuizPool.Count > 0)
             {
+                currentIndex = 0;
                 LoadQuestion();
-            }
-            else
-            {
-                lblQuestion.Text = "No new questions left!";
-                MessageBox.Show("All questions in the database have been used. Delete used_questions.txt to reset.");
             }
         }
 
-        void LoadAllQuestions()
+        //void LoadAllQuestions()
+        //{
+        //    try
+        //    {
+        //        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "questions.json");
+        //        if (File.Exists(path))
+        //        {
+        //            string json = File.ReadAllText(path);
+        //            allGroups = JsonSerializer.Deserialize<List<QuestionGroup>>(json);
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("JSON file not found in Data folder.");
+        //        }
+        //    }
+        //    catch (Exception ex) { MessageBox.Show("JSON Error: " + ex.Message); }
+        //}
+
+        void LoadAllQuestions(string fileName)
         {
             try
             {
-                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "questions.json");
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", fileName);
                 if (File.Exists(path))
                 {
                     string json = File.ReadAllText(path);
@@ -76,7 +146,7 @@ namespace Quiz
                 }
                 else
                 {
-                    MessageBox.Show("JSON file not found in Data folder.");
+                    MessageBox.Show($"{fileName} not found in Data folder.");
                 }
             }
             catch (Exception ex) { MessageBox.Show("JSON Error: " + ex.Message); }
@@ -102,60 +172,86 @@ namespace Quiz
 
             var q = activeQuizPool[currentIndex];
 
-            // Display Question
+            // --- RESET TIMER & PROGRESS BAR ---
+            timeLeft = timer;
+            lblTimer.Text = timer.ToString();
+            lblTimer.ForeColor = Color.Black;
+            PlayAlarm("start_alarm.wav");
+
+            pbTimer.Maximum = timer;
+            pbTimer.Value = timer;
+            pbTimer.Visible = true;
+
+            // Display Question and Options
             lblQuestion.Visible = true;
-            lblQuestion.Text = $"Question {currentIndex + 1}: {q.Text}";
-
+            //lblQuestion.Text = $"Question {currentIndex + 1}: {q.Text}";   // just for testing
+            if (currentIndex < 2)
+            {
+                lblQuestion.Text = $"SAMPLE QUESTION {currentIndex + 1}: {q.Text}";
+            }
+            else
+            {
+                lblQuestion.Text = $"Question {currentIndex - 1}: {q.Text}";
+            }
             string[] prefixes = { "A) ", "B) ", "C) ", "D) " };
-
             for (int i = 0; i < optionLabels.Count; i++)
             {
                 if (i < q.Options.Length)
                 {
                     optionLabels[i].Text = prefixes[i] + q.Options[i];
                     optionLabels[i].Visible = true;
-                    optionLabels[i].ForeColor = Color.Black; // Standard text color
+                    optionLabels[i].ForeColor = Color.Black;
                 }
-                else
-                {
-                    optionLabels[i].Visible = false;
-                }
+                else { optionLabels[i].Visible = false; }
             }
 
             btnNext.Visible = false;
+            btnStop.Visible = false;
             btnShowAnswer.Visible = false;
-            timeLeft = 60;
-            lblTimer.Text = "60";
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            
             timeLeft--;
             lblTimer.Text = timeLeft.ToString();
+
+            if (timeLeft >= 0)
+            {
+                pbTimer.Value = timeLeft;
+            }
+
+            if (timeLeft <= 10)
+            {
+                lblTimer.ForeColor = Color.Red;
+                if (!alarmPlayed)
+                {
+                    PlayAlarm("10sec-countdown.wav");
+                    alarmPlayed = true;
+                }
+            }
 
             if (timeLeft <= 0)
             {
                 timer1.Stop();
-                PlayAlarm();
+                pbTimer.Visible = false;
 
-                // Disappear options so students stop writing
-                lblQuestion.Text = "TIME IS UP!";
-                //foreach (var lbl in optionLabels) lbl.Visible = false;
+                lblQuestion.Text = "TIME'S UP!";
 
                 btnShowAnswer.Visible = true;
             }
         }
 
-        private void PlayAlarm()
+        private void PlayAlarm(string music)
         {
             try
             {
-                string soundPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "alarm.wav");
+                string soundPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", music);
                 if (File.Exists(soundPath))
                 {
                     SoundPlayer player = new SoundPlayer(soundPath);
-                    player.Play(); // Use .PlaySync() if you want the app to wait for the sound
+                    player.Play();
                 }
             }
             catch (Exception ex)
@@ -167,27 +263,113 @@ namespace Quiz
 
         private void btnShowAnswer_Click(object sender, EventArgs e)
         {
-            foreach (var lbl in optionLabels) lbl.Visible = false;
+            DialogResult result = MessageBox.Show("Are you sure you want to SHOW the answer?", "ARE YOU SURE?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
 
-            var q = activeQuizPool[currentIndex];
-            char[] labels = { 'A', 'B', 'C', 'D' };
 
-            // Reveal only the correct answer
-            lblQuestion.Text = $"Correct Answer: {labels[q.CorrectIndex]}) {q.Options[q.CorrectIndex]}";
-            lblQuestion.ForeColor = Color.DarkGreen; // Make the answer stand out
+                foreach (var lbl in optionLabels) lbl.Visible = false;
 
-            File.AppendAllLines(usedFilePath, new[] { q.Id });
-            usedQuestionIds.Add(q.Id);
+                var q = activeQuizPool[currentIndex];
+                char[] labels = { 'A', 'B', 'C', 'D' };
 
-            btnShowAnswer.Visible = false;
-            btnNext.Visible = true;
+                // Reveal only the correct answer
+                lblQuestion.Visible = false;
+                lblCorrectAnswer.Visible = true;
+                lblCorrectAnswer.Text = $"Correct Answer: {labels[q.CorrectIndex]}"; // - {q.Options[q.CorrectIndex]}
+                lblCorrectAnswer.ForeColor = Color.DarkGreen;
+
+                File.AppendAllLines(usedFilePath, new[] { q.Id });
+                usedQuestionIds.Add(q.Id);
+
+                string reportEntry = $"Question : {currentIndex - 1} ID: {q.Id} | Q: {q.Text} | Answer: {labels[q.CorrectIndex]} - {q.Options[q.CorrectIndex]}";
+                sessionResults.Add(reportEntry);
+
+                btnShowAnswer.Visible = false;
+                btnNext.Visible = true;
+                btnStop.Visible = true;
+            }
+        }
+
+        private void SaveSessionReport()
+        {
+            if (sessionResults.Count == 0) return;
+
+            try
+            {
+                string fileName = $"Session_Report_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+                string reportPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", fileName);
+
+                // Add a header
+                List<string> fileContent = new List<string> { "--- QUIZ SESSION REPORT ---", $"Date: {DateTime.Now}\n" };
+                fileContent.AddRange(sessionResults);
+
+                File.WriteAllLines(reportPath, fileContent);
+                MessageBox.Show($"Session report saved to: {fileName}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving report: " + ex.Message);
+            }
         }
 
         private void btnNext_Click(object sender, EventArgs e)
         {
             currentIndex++;
-            lblQuestion.ForeColor = Color.Black; // Reset color for next question
-            LoadQuestion();
+            lblQuestion.ForeColor = Color.Black;
+            alarmPlayed = false;
+            lblCorrectAnswer.Visible = false;
+            if (currentIndex >= activeQuizPool.Count && !isJetpotMode)
+            {
+                lblQuestion.Text = "Session 1 Finished!";
+                btnNext.Visible = true;
+                SaveSessionReport();
+                btnJetpot.Visible = false;
+            }
+            else
+            {
+                LoadQuestion();
+            }
+        }
+
+        private void btnJetpot_Click(object sender, EventArgs e)
+        {
+            isJetpotMode = true;
+            btnJetpot.Visible = false;
+
+            LoadAllQuestions("jetpot_questions.json");
+
+            activeQuizPool = allGroups.SelectMany(g => g.Questions).ToList();
+
+            if (activeQuizPool.Count > 0)
+            {
+                currentIndex = 0;
+                LoadQuestion();
+
+                // Visual cue: Change background to gold for Jetpot!
+                this.BackColor = Color.Gold;
+
+            }
+            else
+            {
+                MessageBox.Show("No questions found in jetpot_questions.json!");
+            }
+        }
+
+        private void btnStartQuiz_Click(object sender, EventArgs e)
+        {
+            panelWelcome.Visible = false; // Hide the welcome screen
+            StartNewSession();           // Now start the actual quiz logic
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit the quiz?", "Exit", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                SaveSessionReport();
+                this.Close();
+            }
         }
     }
 }
